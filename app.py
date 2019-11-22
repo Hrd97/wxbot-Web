fa9dc4b4d8378911c4d2d7204bb133e9d91920d1
Hrd97 <710632511@qq.com>
2019-11-22 15:47:36 +0800

    初步框架

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:////' + os.path.join(base_dir, 'data.sqlite')
+app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+db = SQLAlchemy(app)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0")
